04de9341040acc78533aa91fd2fc5e24da7ccaf8
Chakrit Wichian <chakrit@oozou.com>
2013-09-01 00:51:14 +0700

    Simplified.

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
 
-var express = require('express')
-  , path = require('path')
-  , rack = require('asset-rack')
-  , app = express();
+var path = require('path')
+  , rack = require('asset-rack');
 
-app.use(new rack.Rack(
+new rack.Rack(
   [ new rack.StaticAssets(
     { urlPrefix: '/'
     , dirname: path.resolve(__dirname, 'public')
     })
   ]
-));
+);
 
